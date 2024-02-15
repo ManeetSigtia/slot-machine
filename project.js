175f7287ca95905f1f2baa9a58402f61963553d3
Maneet Sigtia <sigtiamaneet@gmail.com>
2024-02-15 10:59:12 +0000

    Add printRows function

--- project.js
+++ project.js
@@ -54,7 +54,7 @@ const getNumberOfLines = () => {
 const getBet = (balance, lines) => {
     while (true) {
         const bet = prompt("Enter the  bet per line: ");
-        const numberBet = parseFloat(BiquadFilterNode);
+        const numberBet = parseFloat(bet);
 
         if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
             console.log("Invalid bet, try again.")
@@ -73,8 +73,9 @@ const spin = () => {
         }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -87,6 +88,35 @@ const spin = () => {
     return reels;
 }
 
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol;
+            if (i != row.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+
 let balance = deposit();
 const numberOflines = getNumberOfLines();
 const bet = getBet(balance, numberOflines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
